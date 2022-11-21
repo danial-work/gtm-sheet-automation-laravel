a847e86c1d510c39feeeb9c5bff155edc892f07e
unknown <Kasatria@PC-83.ad.kasatria.com>
2022-11-21 15:56:58 +0800

    adding generate dev guide - extra

--- public/js/util-dev-guide.js
+++ public/js/util-dev-guide.js
@@ -27,6 +27,9 @@ function generate_dev_guide(ga)
         let inp_id = $("#inp-dev-guide-id").val();
         let dev_guide_type = $("#inp-dev-guide-type").val();
         let valid_dev_guide_type = ["web","app"];
+        let valid_tab_name = ["overview","tracking","legend"];
+        
+        var sheet_list = [];
 
         console.log(inp_id);
         console.log(dev_guide_type);
@@ -39,8 +42,7 @@ function generate_dev_guide(ga)
 
         if(inp_id)
         {
-            let prepared_inp_id = prepare_inp_id(inp_id)
-            console.log(prepared_inp_id);
+            var prepared_inp_id = prepare_inp_id(inp_id);
 
             let endpoint_sheet = "https://sheets.googleapis.com/v4/spreadsheets/" + prepared_inp_id;
 
@@ -50,14 +52,55 @@ function generate_dev_guide(ga)
                 return data;
             });
             console.log(response);
+            if(response.error)
+                error_alert(response.error.code,response.error.status,response.error.message);
 
             if(typeof response.sheets !== "undefined" && response.sheets)
-                response.sheets.map((sheet) => {
-                    console.log(sheet.properties.sheetId);
-                    console.log(sheet.properties.title);
+            {
+                if(response.properties.title.includes("Tracking Specifications Sheet"))
+                {
+                    console.log(response.properties.title);
                     console.log("-----------------------------------");
-                });
+                    response.sheets.map((sheet) => {
+                        console.log(sheet.properties.sheetId);
+                        console.log(sheet.properties.title);
+                        console.log("-----------------------------------");
+                        if(!valid_tab_name.includes(sheet.properties.title.toLowerCase())){
+                            console.log("invalid tab name");
+                            alert("invalid tab name");
+                            sheet_list = [];
+                            return ;
+                        }
+                        else
+                            sheet_list.push({
+                                spreadsheet_id : prepared_inp_id,
+                                title : sheet.properties.title,
+                                sheet_id : sheet.properties.sheetId,
+                                column_count : sheet.properties.gridProperties.columnCount,
+                                row_count : sheet.properties.gridProperties.rowCount
+                            });
+                    });
+                }
+                else
+                {
+                    console.log("is not Tracking Specifications Sheet");
+                    alert("is not Tracking Specifications Sheet");
+                }
+                console.log(prepared_inp_id);
+                console.log(sheet_list);
+            }
 
+            sheet_list.map(async (sheet) => {
+                endpoint_sheet = "https://sheets.googleapis.com/v4/spreadsheets/" + sheet.spreadsheet_id + "/values/" + sheet.title + "!A1:Z" + sheet.row_count;
+
+                response = await fetch(endpoint_sheet,{method: "get", headers:{Authorization: "Bearer " + ga}})
+                .then((response) => response.json())
+                .then((data) => {
+                    return data;
+                });
+                console.log(response);
+            });
+            
         }
         else
         {
@@ -81,4 +124,14 @@ function prepare_inp_id(inp_id)
         });
     }
     return inp_id;
+}
+
+function error_alert(code, status, message="")
+{
+    alert("Error " + code + " : " + status + "\n" + message);
+    console.log("-----------------------------------");
+    console.log(code);
+    console.log(message);
+    console.log(status);
+    console.log("-----------------------------------");
 }
--- resources/views/auth/google_logout.blade.php
+++ resources/views/auth/google_logout.blade.php
@@ -2,8 +2,12 @@
 require_once base_path()."/public/util/google_setup.php";
 //Reset OAuth access token
 $google_client->revokeToken();
-//Destroy entire session data.
+//Destroy entire session data. ga_at_encode
 session_destroy();
+//Remove Cookie if exist
+if (isset($_COOKIE['ga_at_encode'])) {
+    unset($_COOKIE['ga_at_encode']);
+}
 //redirect page to index.php
 header('location:/new-home');
 ?>
